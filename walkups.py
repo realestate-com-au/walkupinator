f4109b03394e7af11f0fb2b1b53d1ee7d612c1c0
Jarid Spokes <jarid.spokes@rea-group.com>
2015-01-12 04:26:14 +0000

    Added agent recognition and improved code efficiency

--- walkups.py
+++ walkups.py
@@ -3,41 +3,61 @@
 import csv
 import json
 import requests
+assignee = ""
+admins = {
+'***REMOVED***': {'name' : '***REMOVED***', 'id' : ***REMOVED***},
+'***REMOVED***': {'name': '***REMOVED***', 'id': ***REMOVED***},
+'***REMOVED***': {'name': '***REMOVED***', 'id': ***REMOVED***},
+'***REMOVED***': {'name': '***REMOVED***', 'id': ***REMOVED***},
+'***REMOVED***': {'name': '***REMOVED***','id': ***REMOVED***},
+'***REMOVED***': {'name': '***REMOVED***', 'id': ***REMOVED***},
+'***REMOVED***': {'name': '***REMOVED***', 'id': ***REMOVED***},
+'***REMOVED***': {'name': '***REMOVED***', 'id': ***REMOVED***},
+'***REMOVED***': {'name': '***REMOVED***', 'id': ***REMOVED***}}
 
-# saw this used online, will come in handy 
-def prompt(prompt):
-	return raw_input(prompt).strip()
+while True: 
+	# saw this used online, will come in handy 
+	def prompt(prompt):
+		return raw_input(prompt)
 
-# open users file in read mode
-users = open("../userid_pan.csv","r")
+	# open users file in read mode
+	users = open("../userid_pan.csv","r")
 
-# user input
-card = prompt("Swipe your card: ")
-print "Your card number is " + card + "!"
-
-# reads in the csv
-reader = csv.reader(users, delimiter=',')
-for row in reader:
-	if card == row[3]:
-		username = row[1] + " " + row[2]
-		print "Your name is " + username
-		break		
-
-subject = "New Walkup request from: " + username
-body = "Please update this ticket"
-
-# package data in a dictionary matching expected JSON
-data = {'ticket': {'subject': subject, 'comment': {'body': body}, 'requester' : row[0], 'assignee_id': "***REMOVED***"}}
-
-# encode as JSON
-payload = json.dumps(data)
-
-# request parameters
-token = '***REMOVED***'
-url = 'https://***REMOVED***.zendesk.com/api/v2/tickets.json'
-user = "***REMOVED***"
-headers = {'content-type': 'application/json'}
-
-# Do the http POST request
-
-response = requests.post(url, data=payload, auth=("***REMOVED***/token",token), headers=headers)
+	# user input
+	card = str(prompt("Swipe your card: "))
+	card = card[0:20]
+	print "Your card number is " + card + "!"
+	# Fix this to break if an admin swipes #
+	
+	if card in admins:
+		print "Tickets will now be assigned to: " + admins[card]['name']
+		assignee = admins[card]['id']
+		continue
+	else:
+		# reads in the csv
+		reader = csv.reader(users, delimiter=',')
+		for row in reader:
+			if card == row[3]:
+				username = row[1] + " " + row[2]
+				print "Your name is " + username
+				break
+		else:
+			continue
+		subject = "New Walkup request from: " + username
+		body = "This is an auto-generated ticket from the Innovation Hub"
+	
+		# package data in a dictionary matching expected JSON
+		data = {'ticket': {'subject': subject, 'comment': {'body': body}, 'requester' : row[0], 'assignee_id': assignee}}
+	
+		# encode as JSON
+		payload = json.dumps(data)
+	
+		# request parameters
+		token = '***REMOVED***'
+		url = 'https://***REMOVED***.zendesk.com/api/v2/tickets.json'
+		user = "***REMOVED***"
+		headers = {'content-type': 'application/json'}
+	
+		# Do the http POST request
+	
+		response = requests.post(url, data=payload, auth=("***REMOVED***/token",token), headers=headers)
