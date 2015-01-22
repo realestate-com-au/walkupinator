530183fad67a57fa14cb09be2386ced19360f6ee
Jarid Spokes <spokezzy@gmail.com>
2015-01-22 06:35:56 +0000

    Added reload feature, moved user query inside the loop

--- walkups.py
+++ walkups.py
@@ -8,6 +8,7 @@ import pymssql
 jsondata = json.load(open("data.json"))
 assignee = ""
 previouspan = ""
+userexists = True
 # import sql server details from json
 server = jsondata['sqlserver']
 user = jsondata['username'] 
@@ -20,25 +21,30 @@ conn = pymssql.connect(server,user,passw,db)
 # create a cursor to run queries with
 c = conn.cursor(as_dict=True)
 
-# collect initial data
-users = c.execute("SELECT u.Email, u.FName, u.LName, p.PAN " + 
+
+def loaddata():
+    c.execute("SELECT u.Email, u.FName, u.LName, p.PAN " + 
                     "FROM BEARS_Users " +  
                     "AS u JOIN BEARS_UserPAN AS p on u.UserID = p.UserID")
-
 # import api and agent data from data.json
 agents = jsondata['agents']
 walkupyn = jsondata['walkupyn']
 while 1: 
     def prompt(prompt):
         return raw_input(prompt).rstrip()
-
-    
+    loaddata()
+    # Get the userdata from the database
     # user input
     card = str(prompt("Swipe your card: "))
     card = card[0:20]
+    if card == "reload":
+        print "Loaded users from database"
+        loaddata()
+        continue
     if previouspan != card:
         previouspan = card
     else:
+        print "Cannot scan twice in a row"
         continue
     # checks if the card number is an assignee	
     if card in agents:
@@ -53,33 +59,36 @@ while 1:
                 print "Your name is " + username
                 break
         else:
-            print "You are not registered, please register at the printers"
+            userexists = False
+        if userexists == False:
+            print "You are not registered, \nplease register at the printers"
             continue
-        subject = "New Walkup request from: " + username
-        body = "This is an auto-generated ticket from the Innovation Hub"
+        else:
+            subject = "New Walkup request from: " + username
+            body = "This is an auto-generated ticket from the Innovation Hub"
 
-        # package data in a dictionary matching expected JSON
-        data = { 
-        'ticket':{
-            'subject': subject, 
-            'comment':{'body': body},
-            'type': 'task',
-            'requester' : row['Email'], 
-            'assignee_id': assignee,
-            'custom_fields': [{'id':walkupyn, 'value': 'yes'}]
+            # package data in a dictionary matching expected JSON
+            data = { 
+            'ticket':{
+                'subject': subject, 
+                'comment':{'body': body},
+                'type': 'task',
+                'requester' : row['Email'], 
+                'assignee_id': assignee,
+                'custom_fields': [{'id':walkupyn, 'value': 'yes'}]
+                }
             }
-        }
 
-        # encode as JSON
-        payload = json.dumps(data)
+            # encode as JSON
+            payload = json.dumps(data)
 
-        # request parameters
-        token = jsondata['api-token']
-        url = jsondata['api-url']
-        user = jsondata['api-email']
-        headers = {'content-type': 'application/json'}
+            # request parameters
+            token = jsondata['api-token']
+            url = jsondata['api-url']
+            user = jsondata['api-email']
+            headers = {'content-type': 'application/json'}
 
-        # Do the http POST request
+            # Do the http POST request
 
-        response = requests.post(url, data=payload, auth=(jsondata['api-email']
-            + "/token",token), headers=headers)
+            response = requests.post(url, data=payload, auth=(jsondata['api-email']
+                + "/token",token), headers=headers)
